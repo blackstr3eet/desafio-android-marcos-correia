d642e5e14f9ebad846476e712d93c3de2f51b232
Marcos Walker Silva Correia <marquinhos_thuglife@hotmail.com>
2020-03-26 02:17:30 -0300

    create classes and recursos for screens used

---
+++ app/src/main/java/com/ibm/marvel/characters/CharactersActivity.java
@@ -0,0 +1,14 @@
+package com.ibm.marvel.characters;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.ibm.marvel.R;
+
+public class CharactersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_characters);
+    }
+}
---
+++ app/src/main/java/com/ibm/marvel/details/DetailsActivity.java
@@ -0,0 +1,14 @@
+package com.ibm.marvel.details;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.ibm.marvel.R;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
