8bfeb62f4d8ac14e193313c3cb741677a2430eda
Marcos Walker Silva Correia <marquinhos_thuglife@hotmail.com>
2020-03-26 05:10:05 -0300

    create custom action bar

--- app/src/main/java/com/ibm/marvel/details/DetailsActivity.java
+++ app/src/main/java/com/ibm/marvel/details/DetailsActivity.java
@@ -1,7 +1,10 @@
 package com.ibm.marvel.details;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import com.ibm.marvel.R;
 import com.ibm.marvel.details.DetailsModel.DetailsViewModel;
 
@@ -18,12 +21,30 @@ public class DetailsActivity extends AppCompatActivity implements DetailsActivit
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        actionBarCustomSettings();
 
         DetailsConfigurator.INSTANCE.configure(this);
     }
 
     @Override
     public void displayDetailsData(DetailsViewModel viewModel) {
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        onBackPressed();
+        return true;
+    }
+
+    private void actionBarCustomSettings() {
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.custom_action_bar_layout);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back);
+    }
+
+    private void bindViews() {
 
     }
 }
