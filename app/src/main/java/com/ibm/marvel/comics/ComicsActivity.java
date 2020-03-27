c396eb516c730d6fe9cc4af7b6963941360df062
Marcos Walker Silva Correia <marquinhos_thuglife@hotmail.com>
2020-03-27 05:48:18 -0300

    rename some resources for comics

--- app/src/main/java/com/ibm/marvel/comics/ComicsActivity.java
+++ app/src/main/java/com/ibm/marvel/comics/ComicsActivity.java
@@ -1,14 +1,14 @@
-package com.ibm.marvel.headquarters;
+package com.ibm.marvel.comics;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.ibm.marvel.R;
 
-public class HeadquartersActivity extends AppCompatActivity {
+public class ComicsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_headquarters);
+        setContentView(R.layout.activity_comics);
     }
 }
